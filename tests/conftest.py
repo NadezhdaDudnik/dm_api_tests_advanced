f135ef90c706603b163d84b3286995f21129496e
nadin869 <nady86.86@mail.ru>
2024-06-22 17:15:09 +0300

    Добавлены фикстуры авторизации и новый тест на получение пользователя

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,21 +21,35 @@ structlog.configure(
 )
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
     mailhog_client = MailHogApi(mailhog_configuration)
     return mailhog_client
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
     account = DMApiAccount(dm_api_configuration)
     return account
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def auth_account_helper(
+        mailhog_api
+        ):
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+    account = DMApiAccount(dm_api_configuration)
+    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog_api)
+    account_helper.auth_client(
+        login="Jordan22_06_2024_11_05_51",
+        password="123456789"
+    )
+    return account_helper
+
+
+@pytest.fixture(scope="session")
 def account_helper(
         account_api,
         mailhog_api
@@ -44,7 +58,7 @@ def account_helper(
     return account_helper
 
 
-@pytest.fixture()
+@pytest.fixture
 def prepare_user():
     now = datetime.now()
     data = now.strftime("%d_%m_%Y_%H_%M_%S")
