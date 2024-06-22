5bb630af725abc6ce9dd9d6bc3fa526c596ba1bf
nadin869 <nady86.86@mail.ru>
2024-06-22 10:57:25 +0300

    Добавлены фикстуры

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,5 +1,8 @@
+from collections import namedtuple
+from datetime import datetime
 from json import loads
 
+import pytest
 import structlog
 from faker import Faker
 
@@ -19,19 +22,51 @@ structlog.configure(
 )
 
 
-def test_post_v1_account_login():
-    # Регистрация пользователя
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture
+def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
     account = DMApiAccount(dm_api_configuration)
-    mailhog = MailHogApi(mailhog_configuration)
+    return account
 
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
+@pytest.fixture
+def account_helper(
+        account_api,
+        mailhog_api
+):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
+
+
+@pytest.fixture()
+def prepare_user():
+    now = datetime.now()
+    data = now.strftime("%d_%m_%Y_%H_%M_%S")
     fake = Faker("en_US")
-    login = fake.first_name_female() + '12378'
+    login = fake.first_name_female() + f'{data}'
     password = '123456789'
-    email = f'{login}23@mail.ru'
+    email = f'{login}@mail.ru'
+    User = namedtuple("User", ["login", "password", "email"])
+    user = User(login=login, password=password, email=email)
+    return user
+
+
+def test_post_v1_account_login(
+        account_helper,
+        prepare_user
+):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+
+    # Регистрация пользователя
     account_helper.register_new_user(login=login, password=password, email=email)
 
     # Авторизация пользователя
--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -1,5 +1,8 @@
+from collections import namedtuple
+from datetime import datetime
 from json import loads
 
+import pytest
 import structlog
 from faker import Faker
 
@@ -19,20 +22,53 @@ structlog.configure(
 )
 
 
-def test_put_v1_account_email():
-    # Регистрация пользователя
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture
+def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
     account = DMApiAccount(dm_api_configuration)
-    mailhog = MailHogApi(mailhog_configuration)
+    return account
 
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
+@pytest.fixture
+def account_helper(
+        account_api,
+        mailhog_api
+):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
+
+
+@pytest.fixture()
+def prepare_user():
+    now = datetime.now()
+    data = now.strftime("%d_%m_%Y_%H_%M_%S")
     fake = Faker("en_US")
-    login = fake.first_name_female() + '1286'
+    login = fake.first_name_female() + f'{data}'
     password = '123456789'
     email = f'{login}@mail.ru'
     change_email = f'{login}6@mail.ru'
+    User = namedtuple("User", ["login", "password", "email", "change_email"])
+    user = User(login=login, password=password, email=email, change_email=change_email)
+    return user
+
+
+def test_put_v1_account_email(
+        account_helper,
+        prepare_user
+):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+    change_email = prepare_user.change_email
+
+    # Регистрация пользователя
     account_helper.register_new_user(login=login, password=password, email=email)
 
     # Авторизация пользователя
