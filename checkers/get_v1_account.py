79a3a5601276733b599d2ca7e3027be134852b3c
nadin869 <nady86.86@mail.ru>
2024-07-06 23:41:20 +0300

    PyHamcrest проверки метода get_v1_account вынеcены в отдельный модуль

---
+++ checkers/get_v1_account.py
@@ -0,0 +1,70 @@
+from array import array
+from datetime import datetime
+from assertpy import (
+    assert_that,
+    soft_assertions,
+)
+
+from hamcrest import (
+    assert_that,
+    has_property,
+    starts_with,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to,
+    only_contains,
+)
+
+from dm_api_account.models.user_details_envelope import UserRole
+
+
+class GetV1Account:
+    @classmethod
+    def check_response_values(
+            cls,
+            response
+    ):
+        assert_that(
+            response, all_of(
+                has_property(
+                    'resource', has_properties(
+                        {
+                            'settings': has_properties(
+                                {
+                                    "colorSchema": starts_with("Modern"),
+                                    'paging': has_properties(
+                                        {
+                                            "postsPerPage": equal_to(10),
+                                            "commentsPerPage": equal_to(10),
+                                            "topicsPerPage": equal_to(10),
+                                            "messagesPerPage": equal_to(10),
+                                            "entitiesPerPage": equal_to(10)
+                                        }
+                                    )
+                                }
+                            ),
+                            'roles': all_of(
+                                only_contains(
+                                    UserRole.GUEST,
+                                    UserRole.PLAYER,
+                                )
+                            ),
+                            'rating': has_properties(
+                                {
+                                    "enabled": equal_to(True),
+                                    "quality": equal_to(0),
+                                    "quantity": equal_to(0)
+                                }
+                            ),
+                            'online': instance_of(datetime),
+                            'registration': instance_of(datetime),
+                        }
+                    )
+                )
+            )
+        )
+        with soft_assertions():
+            today = datetime.now().strftime('%Y-%m-%d')
+            assert_that(str(response.resource.registration).startswith(today))
+            assert_that(str(response.resource.online).startswith(today))
--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,22 +1,5 @@
-from datetime import datetime
-from assertpy import (
-    assert_that,
-    soft_assertions,
-)
-
-from hamcrest import (
-    # assert_that,
-    has_property,
-    starts_with,
-    all_of,
-    instance_of,
-    has_properties,
-    equal_to,
-    only_contains,
-)
-
 from checkers.http_checkers import check_status_code_http
-from dm_api_account.models.user_details_envelope import UserRole
+from checkers.get_v1_account import GetV1Account
 
 
 def test_get_v1_account_auth(
@@ -39,50 +22,7 @@ def test_get_v1_account_auth(
     )
     with check_status_code_http():
         response = account_helper.dm_account_api.account_api.get_v1_account()
-        # assert_that(
-        #     response, all_of(
-        #         has_property(
-        #             'resource', has_properties(
-        #                 {
-        #                     'settings': has_properties(
-        #                         {
-        #                             "colorSchema": starts_with("Modern"),
-        #                             'paging': has_properties(
-        #                                 {
-        #                                     "postsPerPage": equal_to(10),
-        #                                     "commentsPerPage": equal_to(10),
-        #                                     "topicsPerPage": equal_to(10),
-        #                                     "messagesPerPage": equal_to(10),
-        #                                     "entitiesPerPage": equal_to(10)
-        #                                 }
-        #                             )
-        #                         }
-        #                     ),
-        #                     'login': login,
-        #                     'roles': all_of(
-        #                         only_contains(
-        #                             UserRole.GUEST,
-        #                             UserRole.PLAYER,
-        #                         )
-        #                     ),
-        #                     'rating': has_properties(
-        #                         {
-        #                             "enabled": equal_to(True),
-        #                             "quality": equal_to(0),
-        #                             "quantity": equal_to(0)
-        #                         }
-        #                     ),
-        #                     'online': instance_of(datetime),
-        #                     'registration': instance_of(datetime),
-        #                 }
-        #             )
-        #         )
-        #     )
-        # )
-        with soft_assertions():
-            assert_that(response.resource.login).is_equal_to(login)
-            assert_that(response.resource.online).is_instance_of(datetime)
-            assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
+        GetV1Account.check_response_values(response)
 
 
 def test_get_v1_account_no_auth(
