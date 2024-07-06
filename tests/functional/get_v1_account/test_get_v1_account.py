61f08797ef59c0d1b7001b6ca9c28cbb21566ba2
nadin869 <nady86.86@mail.ru>
2024-07-06 20:17:26 +0300

    Добавлены soft_assertions

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+from assertpy import (
+    assert_that,
+    soft_assertions,
+)
 
 from hamcrest import (
-    assert_that,
+    # assert_that,
     has_property,
     starts_with,
     all_of,
@@ -35,50 +39,54 @@ def test_get_v1_account_auth(
     )
     with check_status_code_http():
         response = account_helper.dm_account_api.account_api.get_v1_account()
-        assert_that(
-            response, all_of(
-                has_property(
-                    'resource', has_properties(
-                        {
-                            'settings': has_properties(
-                                {
-                                    "colorSchema": starts_with("Modern"),
-                                    'paging': has_properties(
-                                        {
-                                            "postsPerPage": equal_to(10),
-                                            "commentsPerPage": equal_to(10),
-                                            "topicsPerPage": equal_to(10),
-                                            "messagesPerPage": equal_to(10),
-                                            "entitiesPerPage": equal_to(10)
-                                        }
-                                    )
-                                }
-                            ),
-                            'login': login,
-                            'roles': all_of(
-                                only_contains(
-                                    UserRole.GUEST,
-                                    UserRole.PLAYER,
-                                )
-                            ),
-                            'rating': has_properties(
-                                {
-                                    "enabled": equal_to(True),
-                                    "quality": equal_to(0),
-                                    "quantity": equal_to(0)
-                                }
-                            ),
-                            'online': instance_of(datetime),
-                            'registration': instance_of(datetime),
-                        }
-                    )
-                )
-            )
-        )
+        # assert_that(
+        #     response, all_of(
+        #         has_property(
+        #             'resource', has_properties(
+        #                 {
+        #                     'settings': has_properties(
+        #                         {
+        #                             "colorSchema": starts_with("Modern"),
+        #                             'paging': has_properties(
+        #                                 {
+        #                                     "postsPerPage": equal_to(10),
+        #                                     "commentsPerPage": equal_to(10),
+        #                                     "topicsPerPage": equal_to(10),
+        #                                     "messagesPerPage": equal_to(10),
+        #                                     "entitiesPerPage": equal_to(10)
+        #                                 }
+        #                             )
+        #                         }
+        #                     ),
+        #                     'login': login,
+        #                     'roles': all_of(
+        #                         only_contains(
+        #                             UserRole.GUEST,
+        #                             UserRole.PLAYER,
+        #                         )
+        #                     ),
+        #                     'rating': has_properties(
+        #                         {
+        #                             "enabled": equal_to(True),
+        #                             "quality": equal_to(0),
+        #                             "quantity": equal_to(0)
+        #                         }
+        #                     ),
+        #                     'online': instance_of(datetime),
+        #                     'registration': instance_of(datetime),
+        #                 }
+        #             )
+        #         )
+        #     )
+        # )
+        with soft_assertions():
+            assert_that(response.resource.login).is_equal_to(login)
+            assert_that(response.resource.online).is_instance_of(datetime)
+            assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
 
 
 def test_get_v1_account_no_auth(
         account_helper
 ):
     with check_status_code_http(401, 'User must be authenticated'):
-        account_helper.dm_account_api.account_api.get_v1_account()
+        account_helper.dm_account_api.account_api.get_v1_account(validate_response=False)
