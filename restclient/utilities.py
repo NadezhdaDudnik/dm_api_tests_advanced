fb349572e5f31be1ec8a4005131999a46922b234
NadezhdaDudnik <nadin869@mail.ru>
2024-07-20 12:52:44 +0300

    Маскирование данных и токена в allure

--- restclient/utilities.py
+++ restclient/utilities.py
@@ -3,6 +3,7 @@ import allure
 import curlify
 import re
 
+# Function to mask sensitive data
 def mask_sensitive_data(data, keys_to_mask):
     if not data:
         return data
@@ -10,28 +11,32 @@ def mask_sensitive_data(data, keys_to_mask):
         return {k: (mask_sensitive_data(v, keys_to_mask) if k not in keys_to_mask else '*****') for k, v in data.items()}
     return data
 
-
+# Function to mask sensitive data in curl commands
 def mask_curl_command(curl_command, keys_to_mask):
     for key in keys_to_mask:
         curl_command = re.sub(rf'("{key}": ")([^"]+)', rf'\1*****', curl_command)
         curl_command = re.sub(rf"('{key}': ')([^']+)", rf'\1*****', curl_command)
+        curl_command = re.sub(rf'({key}=)([^&]+)', rf'\1*****', curl_command)
     return curl_command
-def allure_attach(
-        fn
-        ):
+
+# Decorator for attaching requests and responses to Allure reports with masking
+def allure_attach(fn):
     def wrapper(*args, **kwargs):
         keys_to_mask = ['password', 'login', 'token', 'access_token', 'authorization', 'X-Dm-Auth-Token']
 
+        # Mask the request body
         body = kwargs.get("json")
         if body:
             masked_body = mask_sensitive_data(body, keys_to_mask)
             allure.attach(
                 json.dumps(masked_body, indent=4),
+                name="request_body",
                 attachment_type=allure.attachment_type.JSON,
             )
 
         response = fn(*args, **kwargs)
 
+        # Mask the curl command
         curl = curlify.to_curl(response.request)
         masked_curl = mask_curl_command(curl, keys_to_mask)
         allure.attach(masked_curl, name="curl", attachment_type=allure.attachment_type.TEXT)
@@ -42,7 +47,8 @@ def allure_attach(
             response_text = response.text
             status_code = f"status_code = {response.status_code}"
             allure.attach(
-                response_text if len(response_text) > 0 else status_code, name="response_body",
+                response_text if len(response_text) > 0 else status_code,
+                name="response_body",
                 attachment_type=allure.attachment_type.TEXT
             )
         else:
